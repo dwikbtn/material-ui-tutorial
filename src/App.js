a04a051f5c46aaa5ca806c0bfe77b776119be911
dwikbtn <dwikbtn15@gmail.com>
2021-09-15 09:56:14 +0800

    adding theming

--- src/App.js
+++ src/App.js
@@ -9,7 +9,35 @@ import {
   FormControlLabel,
   TextField,
 } from "@material-ui/core";
+import {
+  makeStyles,
+  ThemeProvider,
+  createTheme,
+} from "@material-ui/core/styles";
+import { orange } from "@material-ui/core/colors";
 
+const useStyle = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #333, #999)",
+    border: 0,
+    borderRadius: 15,
+    color: "white",
+    padding: "0 30px",
+  },
+});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: orange[300],
+    },
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyle();
+
+  return <Button className={classes.root}>Styled btn</Button>;
+}
 function App() {
   const [checked, setChecked] = React.useState(true);
   function CheckBoxExample() {
@@ -27,24 +55,27 @@ function App() {
     );
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <TextField
-          variant="outlined"
-          type="email"
-          placeholder="test@test@gmail.com"
-        />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <ButtonStyled color="primary" />
+          <TextField
+            variant="outlined"
+            type="email"
+            placeholder="test@test@gmail.com"
+          />
 
-        <CheckBoxExample />
-        <ButtonGroup variant="contained" size="large" color="secondary">
-          <Button color="primary" startIcon={<Save />}>
-            Hello from mui
-          </Button>
-          <Button endIcon={<Delete />}>Delete</Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+          <CheckBoxExample />
+          <ButtonGroup variant="contained" size="large" color="secondary">
+            <Button color="primary" startIcon={<Save />}>
+              Hello from mui
+            </Button>
+            <Button endIcon={<Delete />}>Delete</Button>
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
